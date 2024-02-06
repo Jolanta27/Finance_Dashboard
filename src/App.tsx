75b9f1fe13e6ca4a513275ea5f87d0b12b252814
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2024-02-06 10:01:29 +0100

    make navigation header in finance dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { themeSettings  } from './theme';
+import Navbar from '@/scenes/navbar';
 
 
 function App() {
@@ -9,10 +11,18 @@ const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
     <div className="App">
+      <BrowserRouter>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <h1>Hello</h1>
-    </ThemeProvider>
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+    <Routes>
+      <Route path="/" element={<div>dashboard page</div>}/>
+      <Route path="/predictions" element={<div>prediction page</div>}/>
+    </Routes>
+        </Box>
+      </ThemeProvider>
+    </BrowserRouter>
     </div>  
   )
 }
